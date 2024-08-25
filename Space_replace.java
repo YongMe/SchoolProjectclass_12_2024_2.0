6b90ae29a8b059e81e426fb377171e96040f0007
YongMe <92907633+YongMe@users.noreply.github.com>
2024-08-25 14:41:43 +0530

    Update Space_replace.java

--- Space_replace.java
+++ Space_replace.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class SpaceReplacer {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter the string: ");
-        String inputString = scanner.nextLine();
+        String inputString = sc.nextLine();
 
         System.out.print("Enter the character: ");
-        char replacementChar = scanner.next().charAt(0);
+        char replacementChar = sc.next().charAt(0);
 
         String outputString = inputString.replace(' ', replacementChar);
 
