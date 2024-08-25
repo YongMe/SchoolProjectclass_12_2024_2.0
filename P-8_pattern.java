970672cd69d35e0117ee40f2babd2c087300b3ea
YongMe <92907633+YongMe@users.noreply.github.com>
2024-08-25 14:48:45 +0530

    Update P-8_pattern.java

--- P-8_pattern.java
+++ P-8_pattern.java
@@ -1,4 +1,4 @@
-public class PatternPrinter {
+public class Pattern {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
